922201b393c33859375d275a06547cb91f0c671a
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-11-05 16:52:35 +0000

    Sorts odd numbers in a list of odd and even numbers

--- src/main/Kata.java
+++ src/main/Kata.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 public class Kata {
     public static int[] sortArray(int[] array) {
-        return array;
+        Stack <Integer> oddNumbers = new Stack<>();
+
+        Arrays.stream(array)
+                .filter(num -> num % 2 == 1)
+                .map(num -> -num)
+                .sorted()
+                .forEach(oddNumbers::push);
+
+        return Arrays.stream(array)
+                .map(num -> num % 2 == 1 ? -oddNumbers.pop() : num)
+                .toArray();
     }
 }
--- src/test/KataTest.java
+++ src/test/KataTest.java
@@ -10,4 +10,14 @@ class KataTest {
     public void exampleTest2() {
         assertArrayEquals(new int[]{ 1, 7 }, Kata.sortArray(new int[]{ 1, 7 }));
     }
+
+    @org.junit.jupiter.api.Test
+    public void exampleTest3() {
+        assertArrayEquals(new int[]{ 1, 3, 2, 8, 5, 4 }, Kata.sortArray(new int[]{ 5, 3, 2, 8, 1, 4 }));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void exampleTest4() {
+        assertArrayEquals(new int[]{ 1, 3, 5, 8, 0 }, Kata.sortArray(new int[]{ 5, 3, 1, 8, 0 }));
+    }
 }
